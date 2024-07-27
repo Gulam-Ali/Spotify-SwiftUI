b673b0f358644ecf2848c8c48aae8dda03dd0d05
Gulam Ali <gulam2275@gmail.com>
2024-07-27 20:32:34 +0530

    Initial commit

---
+++ Spotify-SwiftUI/Spotify_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_SwiftUIApp.swift
+//  Spotify-SwiftUI
+//
+//  Created by Gulam Ali on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
